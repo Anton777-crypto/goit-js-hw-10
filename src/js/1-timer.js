fbf643cd7d7cbbfca9fc33b9afb9b3888deb4726
Anton2 Spaine <avetrov392@gmail.com>
2024-10-28 12:11:34 -0700

    add markup

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -16,7 +16,6 @@ flatpickr(input, {
 });
 
 function convertMs(ms) {
-  // Number of milliseconds per unit of time
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
