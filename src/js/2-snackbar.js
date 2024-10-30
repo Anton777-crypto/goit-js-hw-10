c6674adc5e11c1a490bf8bf74fd0a716608061bc
Anton2 Spaine <avetrov392@gmail.com>
2024-10-30 14:08:46 -0700

    add markup

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -3,23 +3,30 @@ import iziToast from 'izitoast';
 // Додатковий імпорт стилів
 import 'izitoast/dist/css/iziToast.min.css';
 
-const input = document.querySelector('input[typr="number"]');
-const radio = document.querySelector('input[typr="radio"]');
-const delay = 1000;
+const input = document.querySelector('input[name="delay"]');
+const radio = document.querySelector('input[name="state"]');
+
 const btn = document.querySelector('.btn');
 
 function checkPromise() {
   return new Promise((resolve, reject) => {
     if (input.value > 0 && radio.value('fulfilled')) {
-      setTimeout(() => resolve(`✅ Fulfilled promise in ${delay}ms`), delay);
-    } else setTimeout(() => reject(`❌ Rejected promise in ${delay}ms`), delay);
+      setTimeout(
+        () => resolve(`✅ Fulfilled promise in ${input.value}ms`),
+        input.value
+      );
+    } else
+      setTimeout(
+        () => reject(`❌ Rejected promise in ${input.value}ms`),
+        input.value
+      );
   });
 }
 
 btn.addEventListener('click', event => {
   event.preventDefault();
   if (!document.querySelector('.notification')) {
-    document.body.innerHTML = `<div class="notification"></div>`;
+    document.body.innerHTML += `<div class="notification"></div>`;
   }
   const notification = document.querySelector('.notification');
   checkPromise()
