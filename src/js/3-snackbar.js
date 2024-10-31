0accf7edcad761f10a8d6c92e2abdb4147f0537d
Anton2 Spaine <avetrov392@gmail.com>
2024-10-30 19:24:47 -0700

    add markup

--- src/js/3-snackbar.js
+++ src/js/3-snackbar.js
@@ -3,15 +3,10 @@ import iziToast from 'izitoast';
 // Додатковий імпорт стилів
 import 'izitoast/dist/css/iziToast.min.css';
 
-const input = document.querySelector('input[name="delay"]');
-const radio = document.querySelectorAll('input[name="state"]');
-const form = document.querySelector('.form');
-const btn = document.querySelector('.btn');
-
 form.addEventListener('submit', event => {
   event.preventDefault();
 
-  const value_input = Number(event.target.dalay.value);
+  const value_input = Number(event.target.value_input.value);
   const select_input = event.target.state.value;
 
   const selectPromis = new Promis((resolve, reject) => {
@@ -21,21 +16,22 @@ form.addEventListener('submit', event => {
       reject(`❌ Rejected promise in ${value_input}ms`);
     }
   }, value_input);
+
+  promise
+    .then(value_input => {
+      iziToast.success({
+        title: 'Success',
+        message: `✅ Fulfilled promise in ${delay}ms`,
+        position: 'center',
+        color: 'yelow',
+      });
+    })
+    .capth(value_input => {
+      iziToast.error({
+        title: 'error',
+        message: `✅ Fulfilled promise in ${delay}ms`,
+        position: 'center',
+        color: 'blue',
+      });
+    });
 });
-promise
-  .then(value_input => {
-    iziToast.success({
-      title: 'Success',
-      message: `✅ Fulfilled promise in ${delay}ms`,
-      position: 'center',
-      color: 'yelow',
-    });
-  })
-  .capth(value_input => {
-    iziToast.error({
-      title: 'error',
-      message: `✅ Fulfilled promise in ${delay}ms`,
-      position: 'center',
-      color: 'blue',
-    });
-  });
